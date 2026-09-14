7be86a55506a0a27229bc00eca2230a6515f4f4c
pealar12 <andrew.peal12@gmail.com>
2026-09-14 10:41:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/api/v1/resource', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
